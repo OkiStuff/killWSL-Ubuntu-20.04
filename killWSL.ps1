873a1541760b29841b10b7edf107213bfa800e6f
OkiStuff <40648091+OkiStuff@users.noreply.github.com>
2020-05-02 13:59:28 -0400

    Add files via upload

---
+++ killWSL.ps1
@@ -0,0 +1,15 @@
+﻿
+
+
+
+
+function killWSL {
+
+
+    wsl -t Ubuntu-20.04 
+           #<DistroName>-<VersionNumber>:Remove "-<VersionNumber>" if the WSL Distro you installed does not have the version number in it
+
+    # WARNING THIS FORCE KILLS THE WSL SESSION. FIRST SAVE ALL FILES BEFORE RUNNING THIS COMMAND!
+
+}
+
